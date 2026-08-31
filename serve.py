2e19ed0ffa5625dc8ec47b5a9840a54c5bd0283b
LPFchan <me@lost.plus>
2026-08-31 23:33:03 +0900

    serve: bind 0.0.0.0 so the app is reachable off-box

--- serve.py
+++ serve.py
@@ -11,4 +11,4 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def log_message(self, *a):
         pass
 
-http.server.ThreadingHTTPServer(('127.0.0.1', 8600), Handler).serve_forever()
+http.server.ThreadingHTTPServer(('0.0.0.0', 8600), Handler).serve_forever()
